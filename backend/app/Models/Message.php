39c463d3be026b5ce11c07c32c6c9d1e8d858e19
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2025-12-15 08:16:16 +0000

    add: chat method

---
+++ backend/app/Models/Message.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    public function chat(): BelongsTo
+    {
+        return $this->belongsTo(Chat::class);
+    }
+}
