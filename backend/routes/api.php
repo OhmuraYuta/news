f23c28f75db9a85f83c30a502a699cb59ff03b66
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2025-12-16 02:43:14 +0000

    add: chat and message routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,10 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\MessageController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
@@ -14,6 +12,14 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::resource('chats', ChatController::class)->only([
+        'index', 'store', 'show'
+    ]);
+
+    Route::resource('chats.messages', MessageController::class)->only([
+        'store', 'update'
+    ]);
 });
 
 Route::get('/auth/google/url', [AuthController::class, 'getGoogleAuthUrl']);
