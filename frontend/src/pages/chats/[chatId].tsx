f602ae51b21d92ba850623037223a4c3e804eb76
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2025-12-22 01:49:42 +0900

    fix: スタイル　z index

--- frontend/src/pages/chats/[chatId].tsx
+++ frontend/src/pages/chats/[chatId].tsx
@@ -8,6 +8,9 @@ import { speakMessageHandler } from '@/features/chat/handlers';
 
 import Wrapper from '@/news/components/Wrapper';
 import Chat from '@/news/components/Chat';
+import Login from '@/news/components/Login';
+import NewChatsBtn from '@/news/components/NewChat';
+import HamburgerMenu from '@/news/components/HamburgerMenu';
 
 const Message = () => {
 
@@ -20,9 +23,16 @@ const Message = () => {
   return(
     <div>
       chat id: {chatId}
+      <HamburgerMenu w={10} h={8} />
+      <div className='absolute bottom-60 z-50'>
+        <Chat />
+      </div>
       {modelType === 'vrm' ? <VrmViewer /> : <Live2DViewer />}
-      <Chat />
-      <Wrapper />
+      {/* <Wrapper /> */}
+      <div className='absolute bottom-0 z-50'>
+        <NewChatsBtn />
+        <Login />
+      </div>
     </div>
   )
 };
