d8df9bb4cf6909e4a2ff6f2940ed7532bbbfbafb
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-01-06 23:29:10 +0900

    fix: ハンバーガーメニューのレイアウトを修正し、チャットコンポーネントの表示位置を調整

--- frontend/src/news/components/HamburgerContent.tsx
+++ frontend/src/news/components/HamburgerContent.tsx
@@ -29,8 +29,8 @@ export default function HamburgerContent({isOpen, toggle}: {isOpen: boolean, tog
   }, [isOpen]);
 
   return (
-      <div className="relative h-full w-screen flex">
-        <ul className="relative h-full w-[60vw] bg-white z-[60] transition-all duration-300"
+      <div className="h-full w-screen flex">
+        <ul className="fixed top-0 h-full w-[60vw] bg-white z-[60] transition-all duration-300 pt-20"
           style={{transform: isOpen ? 'translateX(0%)':'translateX(-100%)' }}
         >
           {chats.length != 0 ?
@@ -39,7 +39,7 @@ export default function HamburgerContent({isOpen, toggle}: {isOpen: boolean, tog
             ))
             : 'ログインしてください'}
         </ul>
-        <div className={`bg-black/20 absolute bottom-0 w-full h-full transition-all duration-300
+        <div className={`bg-black/20 fixed bottom-0 w-full h-full transition-all duration-300
           ${isOpen ? 'opacity-100 z-50':'opacity-0 -z-5'}`}
           onClick={toggle}
         >
