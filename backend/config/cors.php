80d86cba1674526121d7f3f32ace4a2d8e46ebd3
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-01-15 11:36:17 +0900

    add: cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://news.ohmura.f5.si'],
 
     'allowed_origins_patterns' => [],
 
