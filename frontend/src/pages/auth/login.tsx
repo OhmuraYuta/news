e640581f19bd6dd36c9c3a98e712c32934adcc38
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-01-22 23:34:11 +0900

    add: ログインページ

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -1,12 +1,17 @@
 'use client'
 
+import Image from 'next/image'
+
+import googleLogo from '@/news/img/google-icon.svg'
 import { handleLogin } from "@/news/utils/auth"
+import { google } from '@ai-sdk/google'
 
 export default function Login() {
   return (
     <div>
-      <h1>Login</h1>
-      <button onClick={handleLogin} style={{ padding: '10px 20px' }}>
+      <h1>tookへようこそ！</h1>
+      <button onClick={handleLogin} className='flex items-center bg-[#3E6EA2]'>
+        <Image src={googleLogo} alt='google logo' />
         Googleでログイン
       </button>
     </div>
