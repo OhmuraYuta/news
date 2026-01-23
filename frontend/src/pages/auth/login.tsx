773754fa8a743e5886c1a8e82adf520313c359c0
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-01-23 09:51:48 +0900

    add: ボタンを押したら押したことがわかるように

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -1,18 +1,32 @@
 'use client'
 
 import Image from 'next/image'
+import { useState } from 'react'
 
 import googleLogo from '@/news/img/google-icon.svg'
 import { handleLogin } from "@/news/utils/auth"
-import { google } from '@ai-sdk/google'
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => {
+    setIsLoading(true);
+    handleLogin();
+  }
   return (
     <div>
       <h1>tookへようこそ！</h1>
-      <button onClick={handleLogin} className='flex items-center bg-[#3E6EA2]'>
-        <Image src={googleLogo} alt='google logo' />
-        Googleでログイン
+      <button onClick={handleClick}
+        className='flex items-center justify-center gap-3 bg-[#3E6EA2] h-[60px] w-4/5 max-w-[400px] mx-auto'
+        disabled={isLoading}
+      >
+        {isLoading ? (
+          <div className="h-5 w-5 animate-spin rounded-full border-2 border-white border-t-transparent" />
+        ) : (
+          <>
+            <Image src={googleLogo} alt='google logo' />
+            Googleでログイン
+          </>
+        )}
       </button>
     </div>
   )
